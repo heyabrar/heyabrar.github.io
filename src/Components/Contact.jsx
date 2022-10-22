3faf587fd4dd3a28e0b3a93ea91ca65948ac5e4e
Abrar Khan <heyabrarkhan@gmail.com>
2022-10-22 18:02:33 +0530

    Contacts modified

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,8 +9,8 @@ export default function Contact ( ){
         <>
         <Text textAlign='center' color='#805AD5' fontSize={{base : '20px',md: '22px', lg : '40px'}} fontWeight={{base : '600', md : '700'}}>Contact</Text>
         <Box pb={{base : '20px', md : '30px'}}>
-            <Text textAlign='center'>Find Me Around The Web</Text>
-            <Flex w={{base :'60%', md : '45%', lg : '25%'}} m='auto' justifyContent='space-around' fontSize={{base : '22px', md : '26px', lg :'30px'}} mt='20px'>
+            <Text textAlign='center' fontSize={{base : '13px', md : '16px'}}>Find Me Around The Web</Text>
+            <Flex w={{base :'60%', md : '35%', lg : '25%'}} m='auto' justifyContent='space-around' fontSize={{base : '22px', md : '26px', lg :'30px'}} mt='20px'>
                 <Box>
                  <a href="https://www.linkedin.com/in/abrar-khan-b67333230/" target='_blank'> <Text><BsLinkedin/></Text></a>
                 </Box>
