99cfb503bb2bd82ba8b94b9b16c62f1a4a9142b4
Abrar Khan <heyabrarkhan@gmail.com>
2022-10-22 10:45:16 +0530

    version 2.0.3 (Rotating Globe Updated)

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -6,11 +6,11 @@ export default function Profile ( ){
         <Box height={{base :'50px', md : '80px'}}></Box>
         <Flex w={{base : '100%', md : '80%'}} m='auto' alignItems='center' justifyContent={{base : 'center', md : 'space-around'}} flexDirection={{base : 'column-reverse', lg : 'row'}} rowGap={{base : '10px', md:'10px'}} mt={{base : '5px', md : '10px', lg : '40px'}}>
             <Box fontSize={{base : '18px', md : '35px', lg : '55px'}} width={{base : '90%', md : '100%', lg : '45%'}} m={{base : 'auto', lg:'0'}} fontWeight='600' textAlign={{base :'center', lg:'left'}}  lineHeight={{base : '28px', md:'50px', lg :'80px'}}>
-                <Flex   justifyContent='center'   gap='5px' direction={{base : 'row', lg :'column'}}>
+                <Flex  justifyContent='center'   gap='5px' direction={{base : 'row', lg :'column'}}>
 
                 <Flex>
                     <Text color='#RGBA(0, 0, 0, 0.64)'><b>Hello World </b></Text>
-                    <Image w={{base : '40px', md : '60px', lg : '80px'}} src="https://cliply.co/wp-content/uploads/2021/02/392102850_EARTH_EMOJI_400px.gif"/>
+                    <Image mt='-5px' w={{base : '40px', md : '60px', lg : '85px'}} src="https://cliply.co/wp-content/uploads/2021/02/392102850_EARTH_EMOJI_400px.gif"/>
                 </Flex>
 
                 <Text color='#RGBA(0, 0, 0, 0.64)'> <b>Myself</b> <b style={{color:'#805AD5'}}>Abrar Khan</b></Text>
