3dfe6435ed40fe1f9f049a578728b4f1efd2cb07
Abrar Khan <heyabrarkhan@gmail.com>
2022-12-05 21:37:20 +0530

    changes

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,12 +5,7 @@ export default function Projects ( ){
     return (
         <>
           <Text textAlign='center' color='#805AD5' fontSize={{base : '20px',md: '22px', lg : '40px'}} fontWeight={{base : '600', md : '700'}}>Projects</Text>
-          <SimpleGrid columns={[1,1,1,2]}  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '15px'}} p={{base : '15px', md :'20px', lg :'25px'}}>
-          <Box className="cardImg" m='auto'>
-                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/201518169-68c77bdd-06c2-44cc-80d4-f4cf0a5981b7.png" alt="Front Img" borderRadius='8px'/>
-                <Image src="https://user-images.githubusercontent.com/103938174/201505966-0ffa2c22-c2fe-4507-abd3-fc54f39b2d17.png" alt="Back Img" className="imgTop" />
-            </Box> 
-
+          <Flex  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '30px', lg :'6%'}} direction={{base : 'column-reverse', lg :'row'}}  p={{base : '15px', md :'20px', lg :'25px'}}>
             <Box m='auto'>
                 <Text fontSize={{base :'14px', md : '18px', lg : '22px'}} fontWeight='600'>ShoppingParadise.com</Text>
                 <Text fontSize={{base : '13px', md : '14px', lg: '16px'}} mt={{base : '5px', md :'10px', lg : '12px'}} lineHeight={{base : '20px', md : '25px', lg : '30px'}}>
@@ -25,10 +20,20 @@ export default function Projects ( ){
                     <Text  fontSize={{base :'20px', md : '25px', lg : '30px'}} ><a href="https://github.com/heyabrar/ShoppingParadise" target='_blank'><AiFillGithub/></a></Text>
                 </Flex>
             </Box>
-         </SimpleGrid>  
+
+            <Box className="cardImg" m='auto'>
+                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/201518169-68c77bdd-06c2-44cc-80d4-f4cf0a5981b7.png" alt="Front Img" borderRadius='8px'/>
+                <Image src="https://user-images.githubusercontent.com/103938174/201505966-0ffa2c22-c2fe-4507-abd3-fc54f39b2d17.png" alt="Back Img" className="imgTop" />
+            </Box>
+         </Flex>  
 
 
          <SimpleGrid columns={[1,1,1,2]}  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '15px', lg : '6%'}} p={{base : '15px', md :'20px', lg :'25px'}}>
+         <Box className="cardImg" m='auto'>
+                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/195987381-d7a8e571-2cd5-43d2-9945-5800aac0819f.png" alt="Front Img" borderRadius='8px'/>
+                <Image src="https://user-images.githubusercontent.com/103938174/193416543-a2f189f0-05d8-4c3e-95d4-96181acda2e1.png" alt="Back Img" class="imgTop" />
+            </Box> 
+
             <Box m='auto'>
                 <Text fontSize={{base :'14px', md : '18px', lg : '22px'}} fontWeight='600'>BeautyBebo.com</Text>
                 <Text fontSize={{base : '13px', md : '14px', lg: '16px'}} mt={{base : '5px', md :'10px', lg : '12px'}} lineHeight={{base : '20px', md : '25px', lg : '30px'}}>Beauty Bebo is India’s fastest growing online retail store for the day to day and special occasion need of the
@@ -42,20 +47,10 @@ export default function Projects ( ){
                     <Text  fontSize={{base :'20px', md : '25px', lg : '30px'}} ><a href="https://github.com/heyabrar/goldy-rake-5952" target='_blank'><AiFillGithub/></a></Text>
                 </Flex>
             </Box>
-
-            <Box className="cardImg" m='auto'>
-                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/195987381-d7a8e571-2cd5-43d2-9945-5800aac0819f.png" alt="Front Img" borderRadius='8px'/>
-                <Image src="https://user-images.githubusercontent.com/103938174/193416543-a2f189f0-05d8-4c3e-95d4-96181acda2e1.png" alt="Back Img" class="imgTop" />
-            </Box> 
          </SimpleGrid>  
 
 
-         <SimpleGrid columns={[1,1,1,2]}  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '30px', lg :'6%'}} p={{base : '15px', md :'20px', lg :'25px'}}>
-            <Box className="cardImg" m='auto'>
-                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/195992334-b4d6c015-e172-4d1f-bee9-448aa2413529.png" alt="Front Img" borderRadius='8px'/>
-                <Image src="https://user-images.githubusercontent.com/103938174/195992296-67899a72-9a18-4849-b02a-d4041104a969.png" alt="Back Img" class="imgTop"/>
-            </Box> 
-
+         <Flex  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '30px', lg :'6%'}} direction={{base : 'column-reverse', lg :'row'}}  p={{base : '15px', md :'20px', lg :'25px'}}>
             <Box m='auto'>
                 <Text fontSize={{base :'14px', md : '18px', lg : '22px'}} fontWeight='600'>CarbManager.com</Text>
                 <Text fontSize={{base : '13px', md : '14px', lg: '16px'}} mt={{base : '5px', md :'10px', lg : '12px'}} lineHeight={{base : '20px', md : '25px', lg : '30px'}}>
@@ -70,32 +65,19 @@ export default function Projects ( ){
                     <Text  fontSize={{base :'20px', md : '25px', lg : '30px'}} ><a href="https://github.com/heyabrar/flashy-burst-4121" target='_blank'><AiFillGithub/></a></Text>
                 </Flex>
             </Box>
-         </SimpleGrid>  
-
-
-         {/* <Flex  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '30px', lg :'6%'}} direction={{base : 'column-reverse', lg :'row'}}  p={{base : '15px', md :'20px', lg :'25px'}}>
-            <Box m='auto'>
-                <Text fontSize={{base :'14px', md : '18px', lg : '22px'}} fontWeight='600'>Shopme.com</Text>
-                <Text fontSize={{base : '13px', md : '14px', lg: '16px'}} mt={{base : '5px', md :'10px', lg : '12px'}} lineHeight={{base : '20px', md : '25px', lg : '30px'}}>
-                Shopme.com is not a clone of an existing website. The design is inspired by multiple clothing sites. 
-                Basically Shopme.com is a E-commerce clothing web application where user gets a collection of mens, women and kids clothing.
-                </Text>
-                <Text fontSize={{base : '12px', md : '14px' , lg :'16px'}} mt={{base : '5px', md :'10px', lg : '12px'}} >An Individual project built and executed  in 1 Day.</Text>
-                <Text fontSize={{base : '14px', md : '18px', lg : '22px'}} mt={{base : '5px', md :'10px', lg : '12px'}} fontWeight='600'>Tech Stack</Text>
-                <Text fontSize={{base : '10px', md : '14px'}} mt={{base : '2px', md :'5px', lg : '2px'}} letterSpacing={{base : '0.5px', lg :'2px'}}>REACT | JAVASCRIPT | CHAKRA UI | ROUTER DOM | NPM</Text>
-                <Flex gap='20px' mt={{base :'10px'}}> 
-                    <Text  fontSize={{base :'20px', md : '25px', lg : '30px'}} ><a href="https://router-project-heyabrar.vercel.app/" target='_blank'><AiFillChrome/></a></Text>
-                    <Text  fontSize={{base :'20px', md : '25px', lg : '30px'}} ><a href="https://github.com/heyabrar/My-Projects/tree/main/Router_Cart_S3_D1/router_project" target='_blank'><AiFillGithub/></a></Text>
-                </Flex>
-            </Box>
 
             <Box className="cardImg" m='auto'>
-                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/195992762-dafd25a9-43db-49a9-b7b0-1d5afc58b931.png" alt="Front Img" borderRadius='8px'/>
-                <Image src="https://user-images.githubusercontent.com/103938174/195992814-a85f24d8-6266-45dd-a925-feb094b5db02.png" alt="Back Img" class="imgTop"/>
+                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/195992334-b4d6c015-e172-4d1f-bee9-448aa2413529.png" alt="Front Img" borderRadius='8px'/>
+                <Image src="https://user-images.githubusercontent.com/103938174/195992296-67899a72-9a18-4849-b02a-d4041104a969.png" alt="Back Img" class="imgTop"/>
             </Box> 
-         </Flex> */}
+         </Flex>  
 
-         <Flex  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '30px', lg :'6%'}} direction={{base : 'column-reverse', lg :'row'}}  p={{base : '15px', md :'20px', lg :'25px'}}>
+         <Flex  w={{base : '85%', md: '80%', lg : '85%'}} m='auto' columnGap='80px' rowGap='25px' mt={{base : '30px', lg :'6%'}} direction={{base : 'column', lg :'row'}}  p={{base : '15px', md :'20px', lg :'25px'}}>
+            <Box className="cardImg" m='auto'>
+                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/194705621-5672200d-fc57-4fd1-9e81-5296e245f4bc.png" alt="Front Img" borderRadius='8px'/>
+                <Image src="https://user-images.githubusercontent.com/103938174/194705661-60d1b0ac-8b4a-4f59-8e9d-1d160da65ce0.png" alt="Back Img" class="imgTop"/>
+            </Box> 
+
             <Box m='auto'>
                 <Text fontSize={{base :'14px', md : '18px', lg : '22px'}} fontWeight='600'>Freshly.com</Text>
                 <Text fontSize={{base : '13px', md : '14px', lg: '16px'}} mt={{base : '5px', md :'10px', lg : '12px'}} lineHeight={{base : '20px', md : '25px', lg : '30px'}}>
@@ -110,11 +92,6 @@ export default function Projects ( ){
                     <Text  fontSize={{base :'20px', md : '25px', lg : '30px'}} ><a href="https://github.com/dineshjangid03/magical-cat-4738" target='_blank'><AiFillGithub/></a></Text>
                 </Flex>
             </Box>
-
-            <Box className="cardImg" m='auto'>
-                <Image  m='auto' src="https://user-images.githubusercontent.com/103938174/194705621-5672200d-fc57-4fd1-9e81-5296e245f4bc.png" alt="Front Img" borderRadius='8px'/>
-                <Image src="https://user-images.githubusercontent.com/103938174/194705661-60d1b0ac-8b4a-4f59-8e9d-1d160da65ce0.png" alt="Back Img" class="imgTop"/>
-            </Box> 
          </Flex>
         </>
     )
