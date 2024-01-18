2e1c50004e5986504500b69874a4f79cb16081af
heyabrar <heyabrarkhan@gmail.com>
2024-01-18 18:36:21 +0530

    refactor: Projects UI change

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,17 @@
 import { create } from "zustand";
-import { IStore } from "../interfaces";
+import { IProjectModalDetails, IStore } from "../interfaces";
+import { initialProjectData } from "../utils/constants";
 
+type x = {
+  description: string;
+  description1: string;
+};
 const useStore = create<IStore>((set) => ({
   darkMode: true,
+  projectData: initialProjectData,
   setDarkMode: (val: boolean) => set(() => ({ darkMode: val })),
+  setProjectData: (val: IProjectModalDetails) =>
+    set(() => ({ projectData: val })),
 }));
 
 export default useStore;
